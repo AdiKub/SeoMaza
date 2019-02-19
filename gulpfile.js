e1432c81225e98236115e803830380aa53dbef01
Пользователь <ADiletkub@gmail.com>
2019-02-19 20:16:05 +0600

    add_gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const {series, src, dest, parallel} = require('gulp');
+const cleaner = require('gulp-clean');
+const sass = require('gulp-sass'); 
+sass.compiler = require('node-sass');
+
+const clean = () =>{
+    return src('dist', {read: false})
+        .pipe(cleaner());
+}
+
+function css() {
+    return src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./dist/css'));
   }
   
-  exports.default = defaultTask
+function html() {
+    return src('./js/**/*.js')
+    .pipe(dest('./dist/'));
+  }
+
+function js() {
+    return src('./templates/**/*.html')
+    .pipe(dest('./dist/js'));
+  }
+ 
+
+  exports.css = css;
+  exports.clean = clean;
+  exports.default = parallel( css);
