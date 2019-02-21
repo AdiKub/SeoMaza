7d276911665ae0786d427cefebdca3936269a814
Пользователь <ADiletkub@gmail.com>
2019-02-21 19:38:05 +0600

    add_fontawesone_and_fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,57 @@
 const {series, src, dest, parallel, watch} = require('gulp');
-const cleaner = require('gulp-clean');
 const sass = require('gulp-sass'); 
+const del = require('del');
 sass.compiler = require('node-sass');
+const googleWebFonts = require('gulp-google-webfonts');
 
-const clean = () =>{
-    return src('dist', {read: false})
-    .pipe(cleaner());
+
+
+
+
+
+const clean = () => del(['dist'])
+
+const css =()=> src('./sass/**/*.scss')
+    .pipe(sass({
+        includePaths: require('node-normalize-scss').includePaths
+      }).on('error', sass.logError))
+    .pipe(dest('./dist/css'));
+  
+const html=()=> src('./templates/**/*.html').pipe(dest('./dist/'));
+
+const js =()=> src('./js/**/*.js').pipe(dest('./dist/js'));
+  
+const imgs=()=> src('./images/**/*').pipe(dest('./dist/images'));
+
+const fortawesome =()=> {
+  return src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+  .pipe(dest('./dist/webfonts/'));
 }
 
-function css() {
-    return src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./dist/css'));
-  }
-  
-function html() {
-    return src('./templates/**/*.html')
-    .pipe(dest('./dist/'));
-  }
 
-function js() {
-    return src('./js/**/*.js')
-    .pipe(dest('./dist/js'));
-  }
-  
-function imgs() {
-    return src('./images/**/*')
-    .pipe(dest('./dist/images'));
-  }
 
-const dev = () =>{
+const watchFile = () =>{
     watch('./sass/**/*.scss', css)
     watch('./js/**/*.js', js)
     watch('./images/**/*', imgs)
     watch('./templates/**/*.html', html)
 }
-  exports.imgs = imgs; 
-  exports.html = html;
-  exports.js = js;
-  exports.css = css;
+
+const fonts = () => {
+     return src('./fonts.list')
+		.pipe(googleWebFonts({
+      fontsDir: 'fonts/',
+      cssDir: 'css/',
+      cssFilename: 'googleFonts.css',
+      relativePaths: true
+    }))
+		.pipe(dest('./dist'));
+  }
+
+const build = series(clean, parallel(js, html, css, imgs, fortawesome, fonts));
+const runDev = series(build, watchFile)
+
   exports.clean = clean;
-  exports.dev = dev;
-  exports.default = series(clean, parallel(js, html, css, imgs));
+  exports.runDev = runDev;
+  exports.build = build;
+  exports.default = build;
