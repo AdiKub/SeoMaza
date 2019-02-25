3574d9e32bf9f283fd7f0b555252ffa5e8f9294d
Пользователь <ADiletkub@gmail.com>
2019-02-25 15:00:16 +0600

    finished_until_input_checker

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,55 @@ const sass = require('gulp-sass');
 const del = require('del');
 sass.compiler = require('node-sass');
 const googleWebFonts = require('gulp-google-webfonts');
+const browsersync = require("browser-sync").create();
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const rename = require('gulp-rename');
+
+
+
+
+const browserSync = (done) => {
+  browsersync.init({
+    server: {
+      baseDir: "./dist/",
+      index: "home.html"
+    }
+  });
+  done();
+}
+
+const browserSyncReload = (done) => {
+  browsersync.reload();
+  done();
+}
+
 
 const clean = () => del(['dist'])
 
-const css =()=> src('./sass/**/*.scss')
+const css =()=> {
+return src(['./sass/**/*.scss', ])
     .pipe(sass({
         includePaths: require('node-normalize-scss').includePaths
       }).on('error', sass.logError))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(autoprefixer())
+    .pipe(cssnano({
+      discardComments: {
+        removeAll: true,
+      }
+    }))
     .pipe(dest('./dist/css'));
-  
+  }
+
+
+  // const nimifyGoogleFonts =() => {
+  //   return src('./dist/css/googleFonts.css')
+  //   .pipe()
+  // }
+
+
+
 const html=()=> src('./templates/**/*.html').pipe(dest('./dist/'));
 
 const js =()=> src('./js/**/*.js').pipe(dest('./dist/js'));
@@ -28,6 +68,7 @@ const watchFile = () =>{
     watch('./js/**/*.js', js)
     watch('./images/**/*', imgs)
     watch('./templates/**/*.html', html)
+    watch('./dist', browserSyncReload)
 }
 
 const fonts = () => {
@@ -42,9 +83,10 @@ const fonts = () => {
   }
 
 const build = series(clean, parallel(js, html, css, imgs, fortawesome, fonts));
-const runDev = series(build, watchFile)
+const runDev = series(build, parallel(browserSync, watchFile));
 
 exports.clean = clean;
 exports.runDev = runDev;
 exports.build = build;
 exports.default = build;
+
